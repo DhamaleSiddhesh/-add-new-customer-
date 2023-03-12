65c3e92b37fb74a7e4d04bea31498d1ac9648d2f
DhamaleSiddhesh <dhamalesiddhesh07@gmail.com>
2023-03-12 18:36:59 +0530

    customerDetails

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="center">
+      <div class="box">
+        <div class="title">Add a new customer</div><br />
+        <div class="title">Customer name</div>
+        <div><input type="text" placeholder="type here.." /></div><br />
+        <div class="title">Customer email</div>
+        <div><input type="text" placeholder="type here.." /></div><br />
+        <div class="title">Buisness ID</div>
+        <div><input type="text" placeholder="type here.." /></div><br />
+        <div class="title">Billing adress</div>
+        <div ><input type="text" placeholder="type here.." /></div><br />
+        <button class = "button">Create</button>
+      </div>
     </div>
   );
 }
